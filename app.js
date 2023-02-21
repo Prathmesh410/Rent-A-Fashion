13d728a2536d524ca1be1c24c817405782f0c4e2
Prathemsh <prathmeshsonawane09538@gmail.com>
2023-02-21 14:17:47 +0530

    Authentication toutes added. Password encryption bbefore storage added.

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-
 const port =  8000  ;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+
+//imports
+const authRoutes = require("./Routes/auth");
+
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+//Routes
+app.use("/api",authRoutes);
+
+
+//Database Connection
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex : true
+}).then(() =>{
+    console.log("DB Connected")
+}).catch(() => {
+    console.log("DB not Connected")
+});
+
 
  //Starting a Server
-app.get("/",(req,res) =>{
-    return res.send("get req");
-})
-
-
- app.listen(port,() =>{
+ app.listen(process.env.PORT,() =>{
      console.log(`app is running at ${port}`);
  });
+
